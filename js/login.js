257d605f745f1b85abd8d266574a972cf3f62204
texacoson <jani.hyrkas@gmail.com>
2019-04-26 13:06:48 +0300

    Worked on global feed, and added forms to login and registration. Simple validation to login and registration.

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const loginEvent = (thisEvent) => {
+  let list = document.querySelectorAll('input');
+  for (let i = 0; i < list.length; i++) {
+    let current = list[i];
+    if (current.name === 'password' || current.name === 'username') {
+      if (current.value === '') {
+        thisEvent.preventDefault();
+        return false;
+      }
+    }
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-const registerUser = () => {
-
-};
-
-const logIn = () => {
-
-};
-
 window.onload = () => {
-const feed = document.getElementById()
+
+  for (let i = 0; i < 10; i++) {
+    let article = document.createElement('article');
+    article.classList.add('feed-box');
+    let text = document.createElement('header');
+    text.innerText = 'One global feed element placeholder.';
+    const pic = document.createElement('IMG');
+    pic.src = 'https://images.pexels.com/photos/259803/pexels-photo-259803.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+    article.appendChild(pic);
+    article.appendChild(text);
+    document.getElementById('global-feed').appendChild(article);
+  }
 };
