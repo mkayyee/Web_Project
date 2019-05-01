c22ac064561a54183e31c861b62b44992fd4c65f
Oona <oona.e.rauhala@gmail.com>
2019-05-01 17:26:01 +0300

    Tiny change to profile js

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const main = document.querySelector('profile-content');
+let main = document.getElementById('profile-content');
 
 console.log(main);
 
@@ -8,5 +8,8 @@ const profilePic = document.createElement('img');
 profilePic.src = 'https://66.media.tumblr.com/ffff32391908e3fc6b90ab7b71783f29/tumblr_pa8jsqxoUr1x1ko3mo1_400.jpg';
 
 console.log(profilePic);
+
+//let article = document.createElement('article');
+//article.appendChild(profilePic);
 
 main.appendChild(profilePic);
