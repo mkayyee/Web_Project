56904aa48c81757c6aaa324539f90f87f72980a7
Oona <oona.e.rauhala@gmail.com>
2019-05-03 17:30:30 +0300

    worked on profile page html/js/css

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,21 @@
 'use strict';
 
-let main = document.getElementById('profile-content');
+//NEXT: get profile photo and username side by side
 
-console.log(main);
+//select profile-content element as main
+const main = document.getElementById('profile-box');
 
+//create profileHeader
+const profileHeader = document.createElement('div');
+main.appendChild(profileHeader);
+
+//create profile photo
 const profilePic = document.createElement('img');
 profilePic.src = 'https://66.media.tumblr.com/ffff32391908e3fc6b90ab7b71783f29/tumblr_pa8jsqxoUr1x1ko3mo1_400.jpg';
+profileHeader.appendChild(profilePic);
 
-console.log(profilePic);
+//create username
+const username = document.createElement('p');
+username.innerText = 'MyUsername';          //Username from db
+profileHeader.appendChild(username);
 
-//let article = document.createElement('article');
-//article.appendChild(profilePic);
-
-main.appendChild(profilePic);
